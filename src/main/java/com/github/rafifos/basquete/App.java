fc8d04d7805837918899dcb701832916f2672999
Rafael Julio <hello.rjulio@gmail.com>
2019-10-14 20:52:07 -0300

    :sparkles: implement Player data insertion and smallest and tallest Player logic

--- src/main/java/com/github/rafifos/basquete/App.java
+++ src/main/java/com/github/rafifos/basquete/App.java
@@ -7,6 +7,10 @@
 
 package com.github.rafifos.basquete;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -14,6 +18,35 @@ import java.util.Scanner;
  * temporada.
  */
 public class App {
+  private static Scanner stdin = new Scanner(System.in);
+  private static String option = "0";
+  private static List<Player> players = new ArrayList<>();
+
+  /**
+   * Ponto de início da aplicação.
+   *
+   * @param args - Os argumentos do programa. Atualmente não utilizado.
+   */
+  public static void main(String[] args) {
+    do {
+      switch (option) {
+      case "1":
+        handlePlayerData();
+        break;
+      case "4":
+        showSmallestAndHighestPlayer();
+        break;
+      case "6":
+        exitProgram(0, true);
+        break;
+      default:
+        printMenu();
+        option = stdin.next();
+        break;
+      }
+    } while (true);
+  }
+
   /**
    * Limpa o buffer de saída do terminal/console. Equivalente ao `clear` ou `cls`.
    */
@@ -23,14 +56,25 @@ public class App {
   }
 
   /**
+   * Reseta a opção a ser digitada pelo usuário, isso é usado para retornar ao
+   * menu.
+   */
+  private static void setDefaultOption() {
+    option = "0";
+  }
+
+  /**
    * Saí do programa.
    *
-   * @param scanner  - Objeto do tipo Scanner que será fechado.
-   * @param exitCode - Código de saída para o programa retornar ao SO.
+   * @param exitCode          - Código de saída para o programa retornar ao SO.
+   * @param shouldClearScreen - Se o programa deve ou não limpar a tela depois de
+   *                          sair.
    */
-  private static void exitProgram(Scanner scanner, int exitCode) {
-    scanner.close();
-    clearScreen();
+  private static void exitProgram(int exitCode, boolean shouldClearScreen) {
+    stdin.close();
+    if (shouldClearScreen) {
+      clearScreen();
+    }
     System.out.println("Encerrando.");
     System.exit(exitCode);
   }
@@ -39,7 +83,6 @@ public class App {
    * Imprime o menu principal.
    */
   private static void printMenu() {
-    clearScreen();
     System.out.println("======== TIME DE BASQUETE ========");
     System.out.println("1 – Dados dos jogadores");
     System.out.println("2 – Média de alturas");
@@ -51,25 +94,63 @@ public class App {
   }
 
   /**
-   * Ponto de início da aplicação.
-   *
-   * @param args - Os argumentos do programa. Atualmente não utilizado.
+   * Lida com os dados dos jogadores, mais precisamente com o Nome e a Altura.
    */
-  public static void main(String[] args) {
-    Scanner stdin = new Scanner(System.in);
+  private static void handlePlayerData() {
+    String currentName = null;
+    double currentHeight = 0.0;
 
-    String option = "0";
+    for (int i = 0; i < 2; i++) {
+      System.out.print("Digite o nome do Jogador: ");
+      currentName = stdin.next();
 
-    do {
-      switch (option) {
-      case "6":
-        exitProgram(stdin, 0);
-        break;
-      default:
-        printMenu();
-        option = stdin.next();
-        break;
+      System.out.print("Digite a altura do Jogador: ");
+      try {
+        currentHeight = stdin.nextDouble();
+      } catch (InputMismatchException ime) {
+        System.out.println("Formato da altura inválida. Retornando ao menu principal.");
+        try {
+          Thread.sleep(3000);
+        } catch (InterruptedException ie) {
+          ie.printStackTrace();
+          exitProgram(1, false);
+        }
+        setDefaultOption();
+        return;
       }
-    } while (true);
+
+      players.add(new Player(currentName, currentHeight));
+    }
+
+    setDefaultOption();
+  }
+
+  /**
+   * Mostra o Jogador mais baixo e o Jogador mais alto.
+   */
+  private static void showSmallestAndHighestPlayer() {
+    final List<Player> _players = players;
+
+    clearScreen();
+
+    Collections.sort(_players);
+
+    System.out.print("O jogador mais baixo é: ");
+    System.out.print(_players.get(0).getName());
+    System.out.println(", com " + _players.get(0).getHeight() + "m de altura");
+
+    System.out.print("O jogador mais alto é: ");
+    System.out.print(_players.get(_players.size() - 1).getName());
+    System.out.println(", com " + _players.get(_players.size() - 1).getHeight() + "m de altura");
+
+    try {
+      Thread.sleep(5000);
+    } catch (InterruptedException ie) {
+      ie.printStackTrace();
+      exitProgram(1, false);
+    }
+
+    clearScreen();
+    setDefaultOption();
   }
 }
